02ae5f4fc2f134e592c795d52a9aa8bd99d2ad80
saanu011 <37443927+saanu011@users.noreply.github.com>
2023-06-09 14:59:50 +0530

    another solution

--- dp/139. Word Break.py
+++ dp/139. Word Break.py
@@ -17,3 +17,24 @@ class Solution:
                     break
         return dp[n]
 
+
+# BFS
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        from collections import deque
+
+        q = deque([s])
+        visited = set()
+
+        while q:
+            s = q.popleft()
+            for word in wordDict:
+                if s.startswith(word):
+                    new_s = s[len(word):]
+                    if new_s == "":
+                        return True
+                    if new_s not in visited:
+                        visited.add(new_s)
+                        q.append(new_s)
+        return False
+
